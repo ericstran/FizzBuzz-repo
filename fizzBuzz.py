6d26555e30c6f86e1d8b9d18d4d682360f0a476b
ericstran <ericstran.stl@gmail.com>
2021-10-15 18:51:38 -0500

    Created more efficient version of FizzBuzz

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,15 +1,22 @@
 # FizzBuzz
 # Goal: Count from 1-100 and replace multiples of 3 and 5 with "Fizz" and "Buzz" respectively (including if both are multiples).
-# Focus: Make it work.
+# Focus: Make it more efficient
 
-counter = 1
-while counter <= 100:
-    if counter % 3 == 0 and counter % 5 == 0:
-        print("FizzBuzz")
-    elif counter % 3 == 0:
-        print("Fizz")
-    elif counter % 5 == 0:
-        print("Buzz")
-    else:
-        print(counter)
+counter = 0
+multThree = 0
+multFive = 0
+while counter < 100:
+    multThree += 1
+    multFive += 1
     counter += 1
+    string = ""
+    if multThree == 3:
+        string += "Fizz"
+        multThree = 0
+    if multFive == 5:
+        string += "Buzz"
+        multFive = 0
+    if string != "":
+        print(string)
+        continue
+    print(counter)
